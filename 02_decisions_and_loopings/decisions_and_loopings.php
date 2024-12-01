6f983df85cca60956602451df1acc89d39c6ed6d
nayan <www.xil187@gmail.com>
2024-12-01 20:46:50 +0530

    new code file created

---
+++ 02_decisions_and_loopings/decisions_and_loopings.php
@@ -0,0 +1,34 @@
+<?php
+    // $age=18;
+    //note: It is compalsory to write $ symbol before using any varible in php
+    // if($age>=18){
+    //     echo "Eligible for voting.\n";
+    // } else {
+    //     echo "Not eligible for voting.\n";
+    // }
+
+    //PHP comparison operators
+    //1. == operator -> returns true if the values are equal
+    // if(1 == 1){
+    //     echo "True\n";
+    // }
+    //2. === operator -> returns true if values & data types are identical
+    // if(3 === 3){
+    //     echo "True\n";
+    // }
+    //3. != operator -> returns true if both values are not equal
+    // if(2 != 3){
+    //     echo "True\n";
+    // }
+    //4. <> not equal operator -> returns true if both values are not equal
+    // if(2 <> 3){
+    //     echo "True\n";
+    // }
+    //5. !== not identical -> returns true if the values or data types are not identical
+    // if(100 != 50){
+    //     echo "True\n";
+    // }
+    //6.Rest are <,>.,<=,>= as same as other languages
+
+    
+?>
