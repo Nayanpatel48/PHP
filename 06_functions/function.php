baf969d8509ae96e800cf97e98889b83b74cceb1
nayan <www.xil187@gmail.com>
2024-12-04 15:17:55 +0530

    functions completed

---
+++ 06_functions/function.php
@@ -0,0 +1,15 @@
+<?php
+    // function myMessage(){
+    //     echo "Hello, World!\n";
+    // }
+    // myMessage();
+
+    //sum two number function with default value
+    // function sum($num1, $num2=9){
+    //     return $num1+$num2;
+    // }
+    // echo sum(2,4);//6
+    // echo "\n";
+
+    
+?>
